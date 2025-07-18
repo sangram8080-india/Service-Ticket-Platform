ace8e29178e657b73567f2c62007a33a468e6b3e
sangram <sangramgkp2020@gmail.com>
2025-07-18 18:23:36 +0530

    added entity and  packages

---
+++ Backend/service-ticket-tool/src/main/java/com/sts/dto/response/NotificationResponse.java
@@ -0,0 +1,5 @@
+package com.sts.dto.response;
+
+public record NotificationResponse() {
+
+}
---
+++ Backend/service-ticket-tool/src/main/java/com/sts/entity/Employee.java
@@ -0,0 +1,54 @@
+package com.sts.entity;
+
+import java.lang.reflect.AccessFlag.Location;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    @Column
+    private String specialization;
+    
+    @Enumerated(EnumType.STRING)
+    private EmployeeStatus status; // AVAILABLE, BUSY, ON_ROUTE, OFFLINE
+    
+    @OneToMany(mappedBy = "assignedEmployee", cascade = CascadeType.ALL)
+    private List<Ticket> assignedTickets;
+    
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
+    private List<Review> reviews;
+    
+    @OneToOne(mappedBy = "employee", cascade = CascadeType.ALL)
+    private Location currentLocation;
+    
+    @Column
+    private Double averageRating;
+    
+    @Column
+    private Integer totalReviews;
+    
+    // Constructors, getters, setters
+}
