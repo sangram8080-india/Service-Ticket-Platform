14b58ed4daa32565fcbd9bbadd5d4aad434a0d71
sangram <sangramgkp2020@gmail.com>
2025-07-19 16:39:33 +0530

    "updated"

--- Backend/service-ticket-tool/src/main/java/com/sts/repository/UserRepository.java
+++ Backend/service-ticket-tool/src/main/java/com/sts/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.sts.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sts.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Long>{
+	Optional<User> findByEmail(String email);
 
 }
